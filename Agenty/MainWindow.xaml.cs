bfdf4e94dbec520605b56c92be26f6bb9a973340
sagiem <sagiems@gmail.com>
2018-02-07 15:01:01 +0600

    1

---
+++ Agenty/MainWindow.xaml.cs
@@ -0,0 +1,47 @@
+﻿using Microsoft.Win32;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Agenty
+{
+    /// <summary>
+    /// Логика взаимодействия для MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        string file;
+
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void button_Click(object sender, RoutedEventArgs e)
+        {
+            OpenFileDialog openfile = new OpenFileDialog();
+            openfile.ShowDialog();
+            if(openfile.FileName != null)
+            {
+                this.file = openfile.FileName;
+            }
+        }
+
+        private void button1_Click(object sender, RoutedEventArgs e)
+        {
+            Raschet raschet = new Raschet(file);
+            raschet.Exelreader();
+        }
+    }
+}
---
+++ Agenty/Raschet.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Excel = Microsoft.Office.Interop.Excel;
+
+namespace Agenty
+{
+    class Raschet
+    {
+        public string File;
+       
+
+        public Raschet(string file)
+        {
+            this.File=file;
+        }
+        public void Exelreader()
+        {
+            Excel.Application ObjWorkExcel = new Excel.Application(); //открыть эксель
+            Excel.Workbook ObjWorkBook = ObjWorkExcel.Workbooks.Open(File); //открыть файл
+            Excel.Worksheet ObjWorkSheet = (Excel.Worksheet)ObjWorkBook.Sheets[1]; //получить 1 лист
+            var lastCell = ObjWorkSheet.Cells.SpecialCells(Excel.XlCellType.xlCellTypeLastCell);//1 ячейку
+            string[,] list = new string[lastCell.Column, lastCell.Row]; // массив значений с листа равен по размеру листу
+            for (int i = 0; i < lastCell.Column; i++) //по всем колонкам
+                for (int j = 0; j < lastCell.Row; j++) // по всем строкам
+                    list[i, j] = ObjWorkSheet.Cells[j + 1, i + 1].Text.ToString();//считываем текст в строку
+            ObjWorkBook.Close(false, Type.Missing, Type.Missing); //закрыть не сохраняя
+            ObjWorkExcel.Quit(); // выйти из экселя
+            GC.Collect(); // убрать за собой
+        }
+
+
+    }
+}
